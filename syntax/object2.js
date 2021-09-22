f2ebb27ecba82e274420edf0c729194b584222c9
okwngus456 <okwngus456@gmail.com>
2021-09-22 20:52:58 +0900

    20210922 v2

--- syntax/object2.js
+++ syntax/object2.js
@@ -1,4 +1,15 @@
-function f1() {
+var f = function f1() {
     console.log(1+1);
     console.log(1+2);
 }
+
+// var i = if(true) {console.log(1)};
+// var w = while(true) {console.log(1)};
+
+var a = [f];
+a[0]();
+
+var o = {
+    func : f
+}
+o.func();
