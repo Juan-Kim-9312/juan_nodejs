a0426810f1e4778adbc9edbbb778062455a63a0a
okwngus456 <okwngus456@gmail.com>
2021-09-22 19:35:39 +0900

    20210922

---
+++ syntax/string.js
@@ -0,0 +1,4 @@
+console.log(1+1);
+console.log('1'+'1');
+
+console.log('나는 개발자다.'.length);
---
+++ syntax/template.js
@@ -0,0 +1,3 @@
+var name = '김주현';
+var letter = `나는 개발자이다. ${name}\n\n나는 node js를 배워서 다른 좋은 회사에 취직할 수 ${name} 있는 계기를 마련할 것이다. 앞으로도 더욱 발전해서, 스스로 웹 어플리케이션을 ${name}만들 수 있는 능력을 기를 것이다.`;
+console.log(letter);
